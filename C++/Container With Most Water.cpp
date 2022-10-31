51289b2d784479da068866fab695d7912670fb8e
B L A C K F U R Y <93466451+div-hacks@users.noreply.github.com>
2022-10-31 22:49:06 +0530

    Create Container With Most Water.cpp

---
+++ C++/Container With Most Water.cpp
@@ -0,0 +1,19 @@
+
+// leetcode
+// Container With Most Water
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int water=0;
+        
+        int l=0,r=n-1;
+        while(l<r){
+            water = max(water,(r-l) * min(height[l], height[r]));
+            if(height[l] < height[r])l++;
+            else r--;
+        }
+        return water;
+    }
+};
